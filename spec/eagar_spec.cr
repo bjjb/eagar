c73b0e169732da932d8a045f33fdc37e818625d8
bjjb <10744+bjjb@users.noreply.github.com>
2025-12-19 16:06:19 +0000

    Further XDG spec implementation...
    The library now also properly honours the XDG_CONFIG_DIRS environment
    variable, and falls back to using `/etc:/usr/local/etc` if unset.

--- spec/eagar_spec.cr
+++ spec/eagar_spec.cr
@@ -5,50 +5,70 @@ require "../src/eagar"
 describe Eagar do
   it "will look in typical locations for all files" do
     xdg = Eagar.xdg
-    Eagar.globs("x", "y", %w(json yaml)).should eq [
-      Path["/etc/x.{json,yaml}"],
-      Path["/etc/x/y.{json,yaml}"],
-      Path["/etc/x/y.d/*.{json,yaml}"],
-      Path["/usr/local/etc/x.{json,yaml}"],
-      Path["/usr/local/etc/x/y.{json,yaml}"],
-      Path["/usr/local/etc/x/y.d/*.{json,yaml}"],
-      Path["~/.x.{json,yaml}"].expand(home: true),
-      Path["~/.x/y.{json,yaml}"].expand(home: true),
-      Path["~/.x/y.d/*.{json,yaml}"].expand(home: true),
-      xdg / "x.{json,yaml}",
-      xdg / "x/y.{json,yaml}",
-      xdg / "x/y.d/*.{json,yaml}",
-      Path[".x.{json,yaml}"].expand(home: true),
-      Path[".x/y.{json,yaml}"].expand(home: true),
-      Path[".x/y.d/*.{json,yaml}"].expand(home: true),
-      Path[".config/x.{json,yaml}"].expand(home: true),
-      Path[".config/x/y.{json,yaml}"].expand(home: true),
-      Path[".config/x/y.d/*.{json,yaml}"].expand(home: true),
-    ]
+    sys_dirs = Eagar.system_config_dirs
+
+    expected = [] of Path
+
+    # System config directories
+    sys_dirs.each do |dir|
+      expected << dir / "x.{json,yaml}"
+      expected << dir / "x/y.{json,yaml}"
+      expected << dir / "x/y.d/*.{json,yaml}"
+    end
+
+    # User home directory
+    expected << Path["~/.x.{json,yaml}"].expand(home: true)
+    expected << Path["~/.x/y.{json,yaml}"].expand(home: true)
+    expected << Path["~/.x/y.d/*.{json,yaml}"].expand(home: true)
+
+    # XDG config directory
+    expected << xdg / "x.{json,yaml}"
+    expected << xdg / "x/y.{json,yaml}"
+    expected << xdg / "x/y.d/*.{json,yaml}"
+
+    # Current directory
+    expected << Path[".x.{json,yaml}"].expand(home: true)
+    expected << Path[".x/y.{json,yaml}"].expand(home: true)
+    expected << Path[".x/y.d/*.{json,yaml}"].expand(home: true)
+    expected << Path[".config/x.{json,yaml}"].expand(home: true)
+    expected << Path[".config/x/y.{json,yaml}"].expand(home: true)
+    expected << Path[".config/x/y.d/*.{json,yaml}"].expand(home: true)
+
+    Eagar.globs("x", "y", %w(json yaml)).should eq expected
   end
 
   it "defaults to using \"config\" for config file base names" do
     xdg = Eagar.xdg
-    Eagar.globs("x").should eq [
-      Path["/etc/x.{json,yaml,ini}"],
-      Path["/etc/x/config.{json,yaml,ini}"],
-      Path["/etc/x/config.d/*.{json,yaml,ini}"],
-      Path["/usr/local/etc/x.{json,yaml,ini}"],
-      Path["/usr/local/etc/x/config.{json,yaml,ini}"],
-      Path["/usr/local/etc/x/config.d/*.{json,yaml,ini}"],
-      Path["~/.x.{json,yaml,ini}"].expand(home: true),
-      Path["~/.x/config.{json,yaml,ini}"].expand(home: true),
-      Path["~/.x/config.d/*.{json,yaml,ini}"].expand(home: true),
-      xdg / "x.{json,yaml,ini}",
-      xdg / "x/config.{json,yaml,ini}",
-      xdg / "x/config.d/*.{json,yaml,ini}",
-      Path[".x.{json,yaml,ini}"].expand(home: true),
-      Path[".x/config.{json,yaml,ini}"].expand(home: true),
-      Path[".x/config.d/*.{json,yaml,ini}"].expand(home: true),
-      Path[".config/x.{json,yaml,ini}"].expand(home: true),
-      Path[".config/x/config.{json,yaml,ini}"].expand(home: true),
-      Path[".config/x/config.d/*.{json,yaml,ini}"].expand(home: true),
-    ]
+    sys_dirs = Eagar.system_config_dirs
+
+    expected = [] of Path
+
+    # System config directories
+    sys_dirs.each do |dir|
+      expected << dir / "x.{json,yaml,ini}"
+      expected << dir / "x/config.{json,yaml,ini}"
+      expected << dir / "x/config.d/*.{json,yaml,ini}"
+    end
+
+    # User home directory
+    expected << Path["~/.x.{json,yaml,ini}"].expand(home: true)
+    expected << Path["~/.x/config.{json,yaml,ini}"].expand(home: true)
+    expected << Path["~/.x/config.d/*.{json,yaml,ini}"].expand(home: true)
+
+    # XDG config directory
+    expected << xdg / "x.{json,yaml,ini}"
+    expected << xdg / "x/config.{json,yaml,ini}"
+    expected << xdg / "x/config.d/*.{json,yaml,ini}"
+
+    # Current directory
+    expected << Path[".x.{json,yaml,ini}"].expand(home: true)
+    expected << Path[".x/config.{json,yaml,ini}"].expand(home: true)
+    expected << Path[".x/config.d/*.{json,yaml,ini}"].expand(home: true)
+    expected << Path[".config/x.{json,yaml,ini}"].expand(home: true)
+    expected << Path[".config/x/config.{json,yaml,ini}"].expand(home: true)
+    expected << Path[".config/x/config.d/*.{json,yaml,ini}"].expand(home: true)
+
+    Eagar.globs("x").should eq expected
   end
 
   it "loads configuration from all the files it can find" do
@@ -96,6 +116,27 @@ describe Eagar do
     else
       # If not set, should default to ~/.config
       Eagar.xdg.should eq(Path["~/.config"].expand(home: true))
+    end
+  end
+
+  it "uses XDG_CONFIG_DIRS if set, otherwise traditional paths" do
+    # Verify that system config directories respect XDG_CONFIG_DIRS
+    # Should use colon-separated XDG_CONFIG_DIRS if set, otherwise /etc and /usr/local/etc
+
+    globs = Eagar.globs("testapp")
+
+    if xdg_dirs = ENV["XDG_CONFIG_DIRS"]?
+      # If XDG_CONFIG_DIRS is set, globs should include those directories
+      xdg_dirs.split(':').each do |dir|
+        globs.should contain(Path[dir] / "testapp.{json,yaml,ini}")
+      end
+      # And should NOT contain the default paths
+      globs.should_not contain(Path["/etc/testapp.{json,yaml,ini}"])
+      globs.should_not contain(Path["/usr/local/etc/testapp.{json,yaml,ini}"])
+    else
+      # If not set, should use traditional Unix paths
+      globs.should contain(Path["/etc/testapp.{json,yaml,ini}"])
+      globs.should contain(Path["/usr/local/etc/testapp.{json,yaml,ini}"])
     end
   end
 end
--- src/eagar.cr
+++ src/eagar.cr
@@ -13,6 +13,14 @@ module Eagar
   class_property user = Path["~"].expand(home: true)
   class_property xdg = Path[ENV.fetch("XDG_CONFIG_HOME", "~/.config")].expand(home: true)
 
+  def self.system_config_dirs : Array(Path)
+    if xdg_dirs = ENV["XDG_CONFIG_DIRS"]?
+      xdg_dirs.split(':').map { |dir| Path[dir] }
+    else
+      [system, site]
+    end
+  end
+
   def self.configuration(name, secondary = "config", extensions = %w(json yaml ini))
     files(name).reduce({"" => ({} of String => String)}) do |cfg, path|
       cfg.tap do |cfg|
@@ -41,25 +49,34 @@ module Eagar
   def self.globs(name, secondary = "config", extensions = %w(json yaml ini))
     extglob = "{#{extensions.join(',')}}"
     cwd = Path["."].expand(home: true)
-    [
-      system / "#{name}.#{extglob}",
-      system / "#{name}/#{secondary}.#{extglob}",
-      system / "#{name}/#{secondary}.d/*.#{extglob}",
-      site / "#{name}.#{extglob}",
-      site / "#{name}/#{secondary}.#{extglob}",
-      site / "#{name}/#{secondary}.d/*.#{extglob}",
-      user / ".#{name}.#{extglob}",
-      user / ".#{name}/#{secondary}.#{extglob}",
-      user / ".#{name}/#{secondary}.d/*.#{extglob}",
-      xdg / "#{name}.#{extglob}",
-      xdg / "#{name}/#{secondary}.#{extglob}",
-      xdg / "#{name}/#{secondary}.d/*.#{extglob}",
-      cwd / ".#{name}.#{extglob}",
-      cwd / ".#{name}/#{secondary}.#{extglob}",
-      cwd / ".#{name}/#{secondary}.d/*.#{extglob}",
-      cwd / ".config/#{name}.#{extglob}",
-      cwd / ".config/#{name}/#{secondary}.#{extglob}",
-      cwd / ".config/#{name}/#{secondary}.d/*.#{extglob}",
-    ]
+
+    result = [] of Path
+
+    # System config directories (respects XDG_CONFIG_DIRS)
+    system_config_dirs.each do |dir|
+      result << dir / "#{name}.#{extglob}"
+      result << dir / "#{name}/#{secondary}.#{extglob}"
+      result << dir / "#{name}/#{secondary}.d/*.#{extglob}"
+    end
+
+    # User home directory
+    result << user / ".#{name}.#{extglob}"
+    result << user / ".#{name}/#{secondary}.#{extglob}"
+    result << user / ".#{name}/#{secondary}.d/*.#{extglob}"
+
+    # XDG config directory (respects XDG_CONFIG_HOME)
+    result << xdg / "#{name}.#{extglob}"
+    result << xdg / "#{name}/#{secondary}.#{extglob}"
+    result << xdg / "#{name}/#{secondary}.d/*.#{extglob}"
+
+    # Current working directory
+    result << cwd / ".#{name}.#{extglob}"
+    result << cwd / ".#{name}/#{secondary}.#{extglob}"
+    result << cwd / ".#{name}/#{secondary}.d/*.#{extglob}"
+    result << cwd / ".config/#{name}.#{extglob}"
+    result << cwd / ".config/#{name}/#{secondary}.#{extglob}"
+    result << cwd / ".config/#{name}/#{secondary}.d/*.#{extglob}"
+
+    result
   end
 end
